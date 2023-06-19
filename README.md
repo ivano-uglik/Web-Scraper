c906dfadbed7fbd16c85f32647c066bdbff2c9e4
ivano-uglik <ivano.uglik7@gmail.com>
2023-06-19 21:17:50 +0200

    title and image of each article is saved in a .json file

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-# To-do
+# How-to
 
-1. open each article, and get everything from there instead
-2. in each article, extract links to images from selector -> #result > a, getattribute(href) if they are available
+To start, run `node index`.
--- index.js
+++ index.js
@@ -1,35 +1,11 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs/promises");
 
-async function tutorial() {
+async function scrapeScrape() {
   // launch browser, go to new page, go to the website of the school
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto("http://ss-tehnicka-ntesla-vu.skole.hr/");
-
-  // // return naslovi od svih clanaka, tako sto se od "vise" anchor tag-a dobije title, koji je title pojedinacnih clanaka
-  // const naslovi = await page.$$eval(
-  //   "#content > div.clanak > div.srednji_stupac_tijelo.mod_news > div > div > a[title]",
-  //   (titles) => {
-  //     return titles.map((x) => x.getAttribute("title"));
-  //   }
-  // );
-  // // write naslovi.txt with the titles of all elements
-  // await fs.writeFile("naslovi.txt", naslovi.join("\n"));
-
-  // // return source of svih images
-  // const photos = await page.$$eval(
-  //   "#content > div.clanak > div.srednji_stupac_tijelo.mod_news > div > div * img",
-  //   (imgs) => {
-  //     return imgs.map((x) => x.src);
-  //   }
-  // );
-  // // save all images of all articles in imgs folder
-  // for (let i = 0; i < photos.length; i++) {
-  //   // console.log(photos[i]);
-  //   const imagefile = await page.goto(photos[i]);
-  //   await fs.writeFile("imgs/" + i + ".png", await imagefile.buffer());
-  // }
 
   // get the number of artikli, to use later to open each of them and extract info
   const brojClanaka = await page.$$eval(
@@ -39,11 +15,11 @@ async function tutorial() {
     }
   );
 
+  const articles = [];
   // for loop which loops through each clanak
   for (let i = 0; i < brojClanaka.length; i++) {
     // go to each clanak
     await page.goto("http://ss-tehnicka-ntesla-vu.skole.hr" + brojClanaka[i]);
-    console.log("Ovo je clanak " + i + ": " + brojClanaka[i]);
 
     // function which returns the title (textcontent of h2 element) of each clanak
     const getTitle = await page.$$eval(
@@ -52,14 +28,31 @@ async function tutorial() {
         return titles.map((x) => x.textContent);
       }
     );
-    console.log("ovo je title clanka " + i + ": " + getTitle);
 
-    getImgs = await page.$$eval("#result > a", (titles) => {
+    const getImgs = await page.$$eval("#result > a", (titles) => {
       return titles.map((x) => x.getAttribute("href"));
     });
-    console.log("Ovo je image clanka " + i + ": " + getImgs + "\n");
+
+    articles.push({ getTitle, getImgs });
+    console.log(articles);
   }
+
   await browser.close();
+
+  return articles;
 }
 
-tutorial();
+scrapeScrape()
+  .then((data) => {
+    const jsonData = data.map(({ getTitle, getImgs }) => {
+      const article = { getTitle, getImgs };
+      return JSON.stringify(article, null, 2) + "\n";
+    });
+    return fs.writeFile("data.json", jsonData.join(""));
+  })
+  .then(() => {
+    console.log("Data has been written to data.json");
+  })
+  .catch((error) => {
+    console.error(error);
+  });
